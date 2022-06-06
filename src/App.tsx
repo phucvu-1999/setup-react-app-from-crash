7af3eaab2a37f23336ca670496236d6b3a07cc29
Tien Phuc <leesinvn123@gmail.com>
2022-06-06 23:17:44 +0700

    Add config webpack for dev and production environment

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <>
       <h1 className="text">Test Css</h1>
+      <h2>Test {process.env.NODE_ENV}</h2>
       <Header />
       <img src={Image} alt="react-logo" width={100} height={100} />
       <img src={Beverages} alt="logo" width={100} height={100} />
